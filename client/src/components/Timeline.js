50a693f495df479941fab74466610c1586af858f
Suele <suelen.ads.guimaraes@gmail.com>
2019-04-01 00:00:51 -0300

    Adiciona a urls da API utilizada no curso e deixa o componente Foto dinamico.

--- client/src/components/Timeline.js
+++ client/src/components/Timeline.js
@@ -3,17 +3,26 @@ import Foto from './Foto';
 
 export default class Timeline extends Component {
 
-
-  componentDidMount(){
-    fetch('https://instalura-api.herokuapp.com/api/public/fotos/alots')
+  constructor(props) {
+    super(props);
+    this.state = {
+      novasFotos: [],
+    }
   }
 
-    render(){
-        return (
-        <div className="fotos container">
-          <Foto/>
-          <Foto/>
-        </div>            
-        );
-    }
+
+  componentDidMount() {
+    fetch('https://instalura-api.herokuapp.com/api/public/fotos/alots')
+      .then(res => res.json())
+      .then(fotos => { this.setState({ novasFotos: fotos }) })
+      .catch(error => console.log(error))
+  }
+
+  render() {
+    return (
+      <div className="fotos container">
+        {this.state.novasFotos.map((novaFoto => <Foto key={novaFoto.id} foto={novaFoto}/>))}
+      </div>
+    );
+  }
 }
