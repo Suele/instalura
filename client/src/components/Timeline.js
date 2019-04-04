9e57c21fe48f8533a17737fa1a146f7f21b43c3f
Suele <suelen.ads.guimaraes@gmail.com>
2019-04-04 13:50:53 -0300

    Remove os espaços, as aspas duplas e indenta o código.

--- client/src/components/Timeline.js
+++ client/src/components/Timeline.js
@@ -1,27 +1,29 @@
-import React, { Component } from 'react';
-import Foto from './Foto';
+import React, { Component } from "react";
+import Foto from "./Foto";
 
 export default class Timeline extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      novasFotos: [],
-    }
+      novasFotos: []
+    };
   }
 
-
   componentDidMount() {
-    fetch('https://instalura-api.herokuapp.com/api/public/fotos/alots')
+    fetch("https://instalura-api.herokuapp.com/api/public/fotos/rafael")
       .then(res => res.json())
-      .then(fotos => { this.setState({ novasFotos: fotos }) })
-      .catch(error => console.log(error))
+      .then(fotos => {
+        this.setState({ novasFotos: fotos });
+      })
+      .catch(error => console.log(error));
   }
 
   render() {
     return (
       <div className="fotos container">
-        {this.state.novasFotos.map((novaFoto => <Foto key={novaFoto.id} foto={novaFoto}/>))}
+        {this.state.novasFotos.map(novaFoto => (
+          <Foto key={novaFoto.id} foto={novaFoto} />
+        ))}
       </div>
     );
   }
