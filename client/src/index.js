0b4acb1eb5717256dbdde46ae285b0259d06edc0
Suele <suelen.ads.guimaraes@gmail.com>
2019-04-04 12:56:16 -0300

    Adiciona o rotas na aplicação.

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Login from './components/Login';
+import Timeline from './components/Timeline';
 import './css/timeline.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <Route exact path="/" component={Login} />
+    <Route path="/timeline" component={Timeline} />
+  </Router>
+  ,
+  document.getElementById('root'));
 
 serviceWorker.unregister();
