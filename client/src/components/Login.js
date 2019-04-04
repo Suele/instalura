75d119978cd295fbbf0d19e9400b2e3be356c2e4
Suele <suelen.ads.guimaraes@gmail.com>
2019-04-04 13:03:31 -0300

    Adiciona o componente login e conexão com a API que verificação os dados do  login.

---
+++ client/src/components/Login.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import '../css/login.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+export default class Login extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      msg: '',
+    }
+  }
+
+  enviaForm = (e) => {
+    e.preventDefault();
+
+    const requestInfo = {
+      method: 'POST',
+      body: JSON.stringify({ email: this.email.value, senha: this.senha.value }),
+      headers: new Headers({
+        'Content-type': 'application/json'
+      })
+    };
+    console.log(requestInfo);
+    fetch('http://instalura-api.herokuapp.com/api/public/login', requestInfo)
+      .then(response => {
+        response.ok ? response.text() : this.setState({ msg: 'Dados incorretos verifique seu email e senha.' })
+      })
+      .then(tokenUsuario => console.log('token: ', tokenUsuario));
+  }
+
+
+  render() {
+    return (
+      <div>
+        {console.log(this.enviaForm)}
+        <form className="login-box" onSubmit={this.enviaForm}>
+          <h1 className="header-logo">Instalura</h1>
+          <span>{this.state.msg}</span>
+          <input name="email" type="text" placeholder="Email" ref={(inputEmail) => this.email = inputEmail} />
+          <input name="senha" type="password" placeholder="Senha" ref={(inputSenha) => this.senha = inputSenha} />
+          <input type="submit" value="ok" />
+        </form>
+      </div>
+    )
+  }
+}
