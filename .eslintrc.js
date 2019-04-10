9d51ad57bb8a0eec149df508868974e2fd916095
Suelen Guimarães <suelen.ads.guimaraes@gmail.com>
2019-04-10 17:37:09 -0300

    Adiciona o eslint e as configurações ao projeto.

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "node": true,
+    "es6": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "semi": ["error", "always"],
+    "quotes": ["error", "single"],
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
+  }
+};
