54066b68de0166947bcad60d3d329eafb83293cc
Suelen Guimarães <suelen.ads.guimaraes@gmail.com>
2019-04-10 22:10:23 -0300

    Muda as aspas para simples, adiciona a verificação de erro para a rota.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,98 +1,129 @@
-import React from 'react';
-import '../css/login.css';
+import React from "react";
+import { Redirect } from "react-router-dom";
+import auth from "../auth";
+import "../css/login.css";
 
 export default class Login extends React.Component {
   constructor() {
     super();
     this.state = {
-      msg: '',
-      email: '',
-      senha: '',
-      erroEmail: '',
-      erroSenha: '',
+      msg: "",
+      email: "",
+      senha: "",
+      erroEmail: "",
+      erroSenha: "",
+      redirectToReferrer: false
     };
   }
 
-  emailForm = (e) => {
+  login = () => {
+    auth.verifyAuth(() => {
+      this.setState({ redirectToReferrer: true });
+    });
+  };
+
+  emailForm = e => {
     this.setState({ email: e.target.value });
-  }
+  };
 
-  senhaForm = (e) => {
+  senhaForm = e => {
     this.setState({ senha: e.target.value });
-  }
+  };
 
-  validacao = (e) => {
+  validacao = e => {
     e.preventDefault();
     if (!this.state.email && !this.state.senha) {
-      return this.setState({ erroEmail: 'o campo e-mail está vaziu.', erroSenha: 'o campo senha está vaziu.' });
-    }
-    else if (!this.state.email && this.state.senha >= 6) {
-      return this.setState({ erroEmail: 'o campo e-mail está vaziu.', erroSenha: '' });
-    }
-    else if (!this.state.senha) {
-      return this.setState({ erroSenha: 'o campo senha está vaziu.' });
-    }
-    else if (this.state.senha.length > 0 && this.state.senha.length < 6 && !this.state.email) {
-      return this.setState({ erroEmail: 'o campo e-mail está vaziu.', erroSenha: 'o campo senha contém 6 caracteres ou mais.' });
-    }
-    else if (this.state.senha.length > 0 && this.state.senha.length < 6 && this.state.email) {
-      return this.setState({ erroEmail: '', erroSenha: 'o campo senha contém 6 caracteres ou mais.' });
-    }
-    else if (this.state.senha.length >= 6 && !this.state.email) {
-      return this.setState({ erroEmail: 'o campo e-mail está vaziu.', erroSenha: '' });
-    }
-    else if (this.state.senha && this.state.email) {
-      this.setState({ erroEmail: '', erroSenha: '' });
+      return this.setState({
+        erroEmail: "o campo e-mail está vaziu.",
+        erroSenha: "o campo senha está vaziu."
+      });
+    } else if (!this.state.email && this.state.senha >= 6) {
+      return this.setState({
+        erroEmail: "o campo e-mail está vaziu.",
+        erroSenha: ""
+      });
+    } else if (!this.state.senha) {
+      return this.setState({ erroSenha: "o campo senha está vaziu." });
+    } else if (
+      this.state.senha.length > 0 &&
+      this.state.senha.length < 6 &&
+      !this.state.email
+    ) {
+      return this.setState({
+        erroEmail: "o campo e-mail está vaziu.",
+        erroSenha: "o campo senha contém 6 caracteres ou mais."
+      });
+    } else if (
+      this.state.senha.length > 0 &&
+      this.state.senha.length < 6 &&
+      this.state.email
+    ) {
+      return this.setState({
+        erroEmail: "",
+        erroSenha: "o campo senha contém 6 caracteres ou mais."
+      });
+    } else if (this.state.senha.length >= 6 && !this.state.email) {
+      return this.setState({
+        erroEmail: "o campo e-mail está vaziu.",
+        erroSenha: ""
+      });
+    } else if (this.state.senha && this.state.email) {
+      this.setState({ erroEmail: "", erroSenha: "" });
       this.enviaForm(e);
+      this.login();
     }
-  }
+  };
 
-  enviaForm = (e) => {
+  enviaForm = e => {
     e.preventDefault();
 
     const userDate = {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify({
         email: this.state.email,
         senha: this.state.senha
       }),
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json"
       }
     };
 
-    fetch('http://localhost:3001', userDate)
+    fetch("http://localhost:3001/", userDate)
       .then(response => response.json())
       .then(responsejson => {
         if (responsejson) {
-          localStorage.setItem('key_token_login', responsejson.token);
+          localStorage.setItem("key_token_login", responsejson.token);
         }
       });
     console.log(this.state);
-  }
-
+  };
 
   render() {
+    let { from } = this.props.location.state || { from: { pathname: "/" } };
+    let { redirectToReferrer } = this.state;
+
+    if (redirectToReferrer) return <Redirect to={from} />;
+
     return (
       <div>
-        <form className="login-box" onSubmit={this.validacao}>
-          <h1 className="header-logo">Instalura</h1>
+        <form className='login-box' onSubmit={this.validacao}>
+          <h1 className='header-logo'>Instalura</h1>
           <span>{this.state.msg}</span>
           <input
-            name="email"
-            type="email"
-            placeholder="Email"
+            name='email'
+            type='email'
+            placeholder='Email'
             onChange={this.emailForm}
           />
           <span>{this.state.erroEmail}</span>
           <input
-            name="senha"
-            type="password"
-            placeholder="Senha"
+            name='senha'
+            type='password'
+            placeholder='Senha'
             onChange={this.senhaForm}
           />
           <span>{this.state.erroSenha}</span>
-          <input type="submit" value="ok" />
+          <input type='submit' value='ok' />
         </form>
       </div>
     );
