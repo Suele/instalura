65bfa086954d665d06500190c47fc730b7234e19
Suelen Guimarães <suelen.ads.guimaraes@gmail.com>
2019-04-10 22:08:19 -0300

    Retira a rota do login e adiciona na pasta routes no arquivo login.js

--- server/config_server.js
+++ server/config_server.js
@@ -1,13 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
-const config = require('./config/index');
-const router = express.Router();
+const routerLogin = require('./routes/login');
 const app = express();
-
-app.listen(3001, () => {
-  console.log('servidor rodando');
-});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -15,44 +9,14 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
-const login = router.post('/', (req, res) => {
+app.use('/', routerLogin);
 
-  const {
-    email,
-    senha
-  } = req.body;
 
-  console.log(email, senha)
-
-  if ((email === undefined || senha === undefined) || (email === '' || senha === '')) {
-    res.status(401).json({
-      msg: 'email e/ou senha são inválidos.'
-    });
-  } else {
-    const tokenData = {
-      email: 'fulano@email.com',
-      senha: '123456',
-      admin: true,
-    }
-
-    let generationToken = jwt.sign({
-      tokenData
-    }, config.JWT_KEY, {
-      expiresIn: '1m'
-    });
-    res.json({
-      tudoCerto: true,
-      token: generationToken
-    })
-  }
+app.listen(3001, () => {
+  console.log('servidor rodando');
 });
-
-app.use('/', login);
-
-
-module.exports = router;
