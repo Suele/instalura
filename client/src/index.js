9adc9e038c761e054e60c77c30b28afc5eb1294b
Suelen Guimarães <suelen.ads.guimaraes@gmail.com>
2019-04-14 20:17:38 -0300

    Remove o arquivo css da timeline, adiciona o componente Header para aparecer somente após o usuário fizer login.

--- client/src/index.js
+++ client/src/index.js
@@ -2,20 +2,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import "./css/timeline.css";
 import "bootstrap/dist/css/bootstrap.css";
 import Login from "./components/Login";
 import Timeline from "./components/Timeline";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import PrivateRoute from "./components/PrivateRoute";
 import Register from "./components/Register";
+import PrivateRoute from "./components/PrivateRoute";
+import Header from "./components/Header";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route exact path='/' component={Login} />
       <Route path="/register" component={Register} />
-      <PrivateRoute path='/timeline' component={Timeline} />
+      <div>
+        <Header />
+        <PrivateRoute path='/timeline' component={Timeline} />
+      </div>
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
