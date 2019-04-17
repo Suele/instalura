3312094092c5fff3560c7fbfaedab4fa827cfcf5
Suelen Guimarães <suelen.ads.guimaraes@gmail.com>
2019-04-16 23:50:44 -0300

    Adiciona o padrão aspas duplas.

--- server/routes/timeline.js
+++ server/routes/timeline.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  console.log('ola timeline');
+router.get("/", (req, res) => {
+  console.log("ola timeline");
   res.status(200).json({
-    message: 'timeline de news',
+    message: "timeline de news",
   });
 });
 
